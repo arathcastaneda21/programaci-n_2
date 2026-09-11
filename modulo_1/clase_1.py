7438ef145a7aca5a2b1f7dabb69acf02775d387a
Estudiante IUB <Ahestrada@unibarranquilla.edu.co>
2026-09-11 10:17:37 -0500

    refactor: rename variables for contact management and enhance menu options

--- modulo_1/clase_1.py
+++ modulo_1/clase_1.py
@@ -1,7 +1,7 @@
-nombre = []
-telefono = []
-email = []
-direccion = []
+nombres = []
+telefonos = []
+emails = []
+direcciones = []
 
 
 while True:
@@ -11,18 +11,46 @@ while True:
     1. Agregar un contacto
     2. Buscar un contacto
     3. Eliminar un contacto
-    4. Salir
+    4. Ver contacto
+    5. Salir
     """
 
     opcion_elegida = int(input(menu))
 
     if opcion_elegida == 1:
+        nombre = input("Ingrese el nombre: ")
+        nombre = nombre.lower()
+        telefono = input("Ingrese el telefono: ")
+        email = input("Ingrese el email: ")
+        direccion = input("Ingrese la direccion: ")
+        nombres.append(nombre)
+        telefonos.append(telefono)
+        emails.append(email)
+        direcciones.append(direccion)
+        print(f"Contacto {nombre} guardado exitosamente")
         pass
     elif opcion_elegida == 2:
-        pass
+        nombre = input("Ingrese el nombre a buscar: ")
+        if nombre in nombres:
+             indice = nombres.index(nombre.lower())
+             print(f"Nombre: {nombre[indice]}")
+             print(f"Telefono: {telefono[indice]}")
+             print(f"Email: {email[indice]}")
+             print(f"Direccion: {direccion[indice]}")
+             print("="*10)
+        else:
+             print("Contacto no existe")
     elif opcion_elegida == 3:
         pass
     elif opcion_elegida == 4:
+            for nombre,telefono,email,direccion in zip(nombres,telefonos,emails,direcciones):
+                print(f"Nombre: {nombre}")
+                print(f"Telefono: {telefono}")
+                print(f"Email: {email}")
+                print(f"Direccion: {direccion}")
+                print("="*10)
+            pass
+    elif opcion_elegida == 5:
         print("Hasta la vida Baby")
         break
     else:
